ed1a25dc528edd45800ee2458baa2f8866d7edbb
Ankit Seth <ankitjain6377@gmail.com>
2025-08-29 19:40:03 +0530

    E-commerce website

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Navbar from "./Components/Navbar/Navbar"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Shop from "./Pages/Shop"
+import ShopCategory from "./Pages/ShopCategory"
+import Product from "./Pages/Product"
+import Cart from "./Pages/Cart"
+import LoginSignup from "./Pages/Loginsignup"
+import Footer from "./Components/Footer/Footer"
+import men_banner from "./assets/banner.jpg"
+import women_banner from "./assets/women_banner.avif"
+import kids_banner from "./assets/kids_banner.jpg"
+import ShopContextProvider from "./Context/ShopContext";
+function App() {
+ 
+  return (
+    <div >
+      <ShopContextProvider>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Shop/>}/>
+        <Route path="/mens" element={<ShopCategory banner={men_banner} category="men"/> }/>
+        <Route path="/womens" element={<ShopCategory banner={women_banner}category="women"/>}/>
+        <Route path="/kids" element={<ShopCategory banner={kids_banner}category="kid"/>}/>
+        <Route path="/product" element={<Product/>}/>
+        <Route path="/product/:productId" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<LoginSignup/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+      </ShopContextProvider>
+
+    </div>
+     
+  )
+}
+
+export default App
